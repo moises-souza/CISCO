2094b162a972bdc07c68e26e7a6f868a99327feb
moises-souza <moisesssouza35@gmail.com>
2023-08-17 16:38:25 -0300

    Create test1_loop.py

---
+++ test1_loop.py
@@ -0,0 +1,18 @@
+import getpass
+import sys
+import telnetlib
+
+host = "10.10.100.2"
+usuario = raw_input("Entre com o seu usuário: ")
+password = getpass.getpass()
+
+executar_telnet = telnetlib.Telnet(host)
+
+executar_telnet.read_until("username: ")
+executar_telnet.write(usuario + "\n")
+
+for n in range (2,21):
+    print ("Vlan" + str(n))
+    print ("name Vlan_script_" + str())
+
+
