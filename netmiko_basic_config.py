611544b168c8bb243357e9c3341b3797be6c062e
moises-souza <85081477+moises-souza@users.noreply.github.com>
2023-08-23 09:08:12 -0300

    Update netmiko_basic_config.py

--- netmiko_basic_config.py
+++ netmiko_basic_config.py
@@ -2,30 +2,29 @@
 
 from netmiko import ConnectHandler
 
-iosv_12_s1 = {
-    'device type': 'cisco_ios',
+iosv_l2_s1 = {
+    'device_type': 'cisco_ios',
     'ip': '10.10.100.2',
     'username': 'cisco',
     'password': 'cisco',
-
 }
-iosv_12_s2 = {
-    'device type': 'cisco_ios',
+
+iosv_l2_s2 = {
+    'device_type': 'cisco_ios',
     'ip': '10.10.100.4',
     'username': 'cisco',
     'password': 'cisco',
-
 }
 
 with open('basic_config1') as f:
     lines = f.read().splitlines()
 print lines
 
-all_devices = [iosv_12_s1,iosv_12_s2]
+
+all_devices = [iosv_l2_s1, iosv_l2_s2]
 
 for devices in all_devices:
     net_connect = ConnectHandler(**devices)
     output = net_connect.send_config_set(lines)
     print output
 
-    
